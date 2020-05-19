384d78d611a164372d9b1d89ee22b907289bf286
julia.natsuko@gmail.com <julia.natsuko@gmail.com>
2020-05-19 16:32:40 +0200

    Finish admin panel

--- server/helpers/database.js
+++ server/helpers/database.js
@@ -2,15 +2,12 @@ const mongoose = require('mongoose');
 
 const connect = async () => {
     if (mongoose.connection.readyState === 0) {
-        await mongoose.connect(
-            process.env.NODE_ENV === 'test' ? global.__DB_URL__ : process.env.DB_URL,
-            {
-                useNewUrlParser: true,
-                useCreateIndex: true,
-                useFindAndModify: false,
-                useUnifiedTopology: true,
-            }
-        );
+        await mongoose.connect(process.env.DB_URL, {
+            useNewUrlParser: true,
+            useCreateIndex: true,
+            useFindAndModify: false,
+            useUnifiedTopology: true,
+        });
     }
 };
 
@@ -18,7 +15,7 @@ const truncate = async () => {
     if (mongoose.connection.readyState !== 0) {
         const { collections } = mongoose.connection;
 
-        const promises = Object.keys(collections).map(collection =>
+        const promises = Object.keys(collections).map((collection) =>
             mongoose.connection.collection(collection).deleteMany({})
         );
 
